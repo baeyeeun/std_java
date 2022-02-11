acfe8889c3a2fc070682326433149b96ff2986e6
user <user@14.53.248.95>
2022-02-11 14:25:49 +0900

    LL

--- src/com/jjava/lecture/ch04/IfNestedExample.java
+++ src/com/jjava/lecture/ch04/IfNestedExample.java
@@ -6,17 +6,28 @@ public class IfNestedExample {
 	
 		int score = 98;
 		String grade ="";
-		String hakjam="";
 		
 		if(score >= 90) {
 			if(score >= 95) {
 				grade = "A+";
-			} else {
+			} else if (score >= 90) {
 				grade = "A";
-
-		}
+			} else if (score >=85) {
+				grade = "B+";
+			} else if (score >=80) {
+				grade = "B";
+			} else if (score >=75) {
+				grade = "C+";
+			} else if (score >=70) {
+				grade = "C";
+			} else if (score >=65) {
+				grade = "D+";
+			} else if (score >=60) {
+				grade = "D";
+			} else if (score >= 55) {
+				grade = "F";
+			}
 		System.out.println("학점은 :" + grade + "입니다.");
-		System.out.println("학점은 :" + hakjam + "입니다.");
 	}
 	}
 }
